712485734c262894776cbb56dd9716576fa12e21
tupac-rocher <47113639+tupac-rocher@users.noreply.github.com>
2020-01-13 18:54:15 +0100

    Go IA

--- src/main/java/fr/umontpellier/iut/conquest/strategies/Minmax.java
+++ src/main/java/fr/umontpellier/iut/conquest/strategies/Minmax.java
@@ -21,30 +21,40 @@ public class Minmax implements Strategy {
         this.level = level;
     }
 
-    public int minmax(Player player, Board board){
-        return board.getNbPawns(player) - board.getNbPawns(player.getGame().getOtherPlayer(player));
+    public int minimax(Player player, Board board){
+        return maxTurn(board, player) - minTurn(board, player);
+    }
+
+    public int maxTurn(Board board, Player player){
+        int max = 0;
+        //Détermine le max
+        for (Move moveBis : board.getValidMoves(player)) {
+            if(max < board.nbPanwsChanged(moveBis)){
+                max = board.nbPanwsChanged(moveBis);
+            }
+
+        }
+        return max;
+
+    }
+
+    public int minTurn(Board board, Player player){
+        int min = 0;
+        for (Move moveBis : board.getValidMoves(player)) {
+            if(min < board.nbPanwsChanged(moveBis)){
+                min = board.nbPanwsChanged(moveBis);
+            }
+        }
+        return min;
     }
 
     /**
      * Retourne un coup valide à partir du niveau d'intelligence.
      */
     public Move getMove(Board board, Player player) {
-        Move move;
-        int max = 0;
+        int bestChoice;
         if (this.level == 1){
-            //Détermine le max
-            for (Move moveBis : board.getValidMoves(player)) {
-                if(max < board.nbPanwsChanged(moveBis)){
-                    max = board.nbPanwsChanged(moveBis);
-                }
-
-            }
-            //Bouge le premier qui correspond au max
-            for(Move move1 : board.getValidMoves(player)){
-                if(max == board.nbPanwsChanged(move1)){
-                    return move1;
-                }
-            }
+            return getBestMove(minimax(player, board), board, player);
         }
         if (this.level == 2){
 
@@ -57,16 +67,17 @@ public class Minmax implements Strategy {
 
         }
         //Move move = new Move(1,2,3,4);
+        return null;
+    }
 
-        int min = 0;
-        for (Move moveBis : board.getValidMoves(player)) {
-            if(min < board.nbPanwsChanged(moveBis)){
-                min =  board.nbPanwsChanged(moveBis);
-                break;
+    public Move getBestMove(int numberBestChoice, Board board, Player player){
+        //Bouge le premier qui correspond au meilleur choix
+        for(Move move1 : board.getValidMoves(player)){
+            if(numberBestChoice == board.nbPanwsChanged(move1)){
+                return move1;
             }
         }
         return null;
-
     }
 
 }
