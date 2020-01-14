39d39b0868e93ad0713dbc51694ab38cd31a9a89
Numa MAURIN <numa.maurin@gmail.com>
2020-01-14 12:07:25 +0100

    essai IA

--- src/main/java/fr/umontpellier/iut/conquest/strategies/MinMax.java
+++ src/main/java/fr/umontpellier/iut/conquest/strategies/MinMax.java
@@ -1,8 +1,22 @@
 package fr.umontpellier.iut.conquest.strategies;
 
+function minimax(node, depth, maximizingPlayer) is
+        if depth = 0 or node is a terminal node then
+        return the heuristic value of node
+        if maximizingPlayer then
+        value := −∞
+        for each child of node do
+        value := max(value, minimax(child, depth − 1, FALSE))
+        return value
+        else (* minimizing player *)
+        value := +∞
+        for each child of node do
+        value := min(value, minimax(child, depth − 1, TRUE))
+        return value
 
 import fr.umontpellier.iut.conquest.Board;
 import fr.umontpellier.iut.conquest.Move;
+import fr.umontpellier.iut.conquest.Pawn;
 import fr.umontpellier.iut.conquest.Player;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,53 +35,43 @@ public class Minmax implements Strategy {
         this.level = level;
     }
 
+    public int minimax(Player player, Board board, Move move0, int level, boolean currentPlayer){
+        board.movePawn(move0);
+        if(level == 1 ||board.getValidMoves(player).isEmpty()||player.getGame().isFinished()){
+            return  board.getNbPawns(player)-board.getNbPawns(player.getGame().getOtherPlayer(player));
+        }
+        if(currentPlayer){
+            int value = -10000000;
+            for(Move move : board.getValidMoves(player)){
+                value = Math.max(value, minimax(player,new Board(board.deepCopyField()), move, level-1, false));
+            }
+            return value;
+
+        }
+        else{
+            int value = 10000000;
+            for(Move move : board.getValidMoves(player.getGame().getOtherPlayer(player))){
+                value = Math.min(value, minimax(player,new Board(board.deepCopyField()), move, level-1, true));
+            }
+            return value;
+        }
+    }
+
     /**
      * Retourne un coup valide à partir du niveau d'intelligence.
      */
-    /*pseudo code
-    function minimax(node, depth, maximizingPlayer) is
-    if depth = 0 or node is a terminal node then
-        return the heuristic value of node
-    if maximizingPlayer then
-        value := −∞
-        for each child of node do
-            value := max(value, minimax(child, depth − 1, FALSE))
-        return value
-    else (* minimizing player *)
-        value := +∞
-        for each child of node do
-            value := min(value, minimax(child, depth − 1, TRUE))
-        return value
-     */
     public Move getMove(Board board, Player player) {
-        List l = new ArrayList<>();
-        int nbPawns;
-        int max;
-        if (this.level == 1){
-            for (Move move: board.getValidMoves(player)) {
-                nbPawns = board.getNbPawns(player);
-                l.add(nbPawns);
+        int drawValue = 0;
+        int currentDrawValue = 0;
+        Move bestMove = board.getValidMoves(player).get(0);
+        for(Move move : board.getValidMoves(player)){
+            currentDrawValue = minimax(player, new Board(board.deepCopyField()), move, level, true);
+            if(currentDrawValue> drawValue){
+                drawValue = currentDrawValue;
+                bestMove = move;
             }
-            for (int i=0; i<l.size(); i++){
-                int depart = 0;
-                if (depart < l.get(i)){
-                    depart = l.get(i);
-                    max = i;
-                }
-            }
-            return board.getValidMoves(player).get(max);
         }
-        if (this.level == 2){
-
-        }
-        if (this.level == 3){
-
-        }
-        if (this.level == 4){
-
-        }
-        Move move = new Move(1,2,3,4);
-        return move;
+        return bestMove;
     }
 
 }
